2be4f20d6963378893ef4df75db9ff4e33e40ff5
Bruno Girin <brunogirin@gmail.com>
2012-08-10 19:39:02 +0100

    Made paths independent of deployment directory

--- nhtg12/settings.py
+++ nhtg12/settings.py
@@ -1,4 +1,5 @@
 # Django settings for nhtg12 project.
+import inspect, os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -59,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/bruno/Projects/nhtg12/static/'
+STATIC_ROOT = str(os.path.join(inspect.getfile(inspect.currentframe()), 'static/'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -109,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/bruno/Projects/nhtg12/templates'
+    str(os.path.join(inspect.getfile(inspect.currentframe()), '../../templates'))
 )
 
 INSTALLED_APPS = (
