858700191155ea003b630f3974d3ea92d938df2d
Bruno Girin <brunogirin@gmail.com>
2012-08-10 19:47:38 +0100

    Forced directories when building paths

--- nhtg12/settings.py
+++ nhtg12/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = str(os.path.join(inspect.getfile(inspect.currentframe()), 'static/'))
+STATIC_ROOT = str(os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), 'static/'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -110,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    str(os.path.join(inspect.getfile(inspect.currentframe()), '../../templates'))
+    str(os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), '../templates'))
 )
 
 INSTALLED_APPS = (
